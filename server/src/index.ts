42d14dff86483daf6b56ba6c60af14fe1aebd877
ahmed-jamal-dev <dev.ahmed.jamal@gmail.com>
2025-08-26 01:45:21 +0300

    add initial server setup with Express, TypeScript, and nodemon configuration

---
+++ server/src/index.ts
@@ -0,0 +1,21 @@
+import express, { RequestHandler } from 'express';
+const app = express();
+app.use(express.json());
+const RequestLoggerMiddleware: RequestHandler =(req, res, next) => {
+    console.log('new request made:', req.path, '__body:', req.body);
+    next();
+}
+app.use(RequestLoggerMiddleware);
+app.use((req, res, next) => {
+    console.log(Date.now());
+});
+const posts : any [] = [];
+app.get('/posts', (req, res) => {
+    res.send( {posts} );
+});
+app.post('/posts', (req, res) => {
+    const post = req.body;
+    posts.push(post);
+    res.sendStatus(200);
+});
+app.listen(3000);
